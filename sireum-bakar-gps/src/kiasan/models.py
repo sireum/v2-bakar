618ae5702b977fa464aad9ffc57a90f1c36bfb26
jjedrys <jjedrys@santoslab.org>
2013-04-26 14:24:16 -0500

    empty stack frame fix

--- sireum-bakar-gps/src/kiasan/models.py
+++ sireum-bakar-gps/src/kiasan/models.py
@@ -102,10 +102,11 @@ class Method(Entity):
             stack_frame = CaseStateFrame()
             stack_frame._name = stack_frame_dict["location"]["name"]
             stack_frame._line_num = stack_frame_dict["line"]
-                        
-            for variable_name in stack_frame_dict["optBaseElementMap"]:
-                variable_value = stack_frame_dict["optBaseElementMap"][variable_name]["theValue"]
-                stack_frame._variables[variable_name] = variable_value
+            
+            if stack_frame_dict["optBaseElementMap"] != None:                        
+                for variable_name in stack_frame_dict["optBaseElementMap"]:
+                    variable_value = stack_frame_dict["optBaseElementMap"][variable_name]["theValue"]
+                    stack_frame._variables[variable_name] = variable_value
             case_state._frames.append(stack_frame)
             
         return case_state
