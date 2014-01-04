56450585feea3f7188904bdc474a8aefff53292b
zhi <zhangzhi@ksu.edu>
2014-01-04 17:33:22 -0600

    empty SIFL

--- sireum-bakar-sifl/src/main/scala/org/sireum/bakar/sifl/module/BakarSiflConfig.scala
+++ sireum-bakar-sifl/src/main/scala/org/sireum/bakar/sifl/module/BakarSiflConfig.scala
@@ -1,28 +1,28 @@
-package org.sireum.bakar.sifl.module
-
-import org.sireum.alir.ControlFlowGraph
-import org.sireum.pipeline.Input
-import org.sireum.pipeline.Produce
-import org.sireum.option.PipelineMode
-import org.sireum.pipeline.gen.ModuleGenerator
-
-case class BakarSiflConfig (
-    title : String = "SIFL information flow analysis tool",
-    
-    @Input
-    cfg : Boolean, //ControlFlowGraph[Boolean], 
-    
-    @Produce
-    siflResults : Boolean
-)
-
-object BakarSiflModuleGen {
-  def main(args: Array[String]) {
-    val opt = PipelineMode()
-    opt.classNames = Array(BakarSiflConfig.getClass().getName().dropRight(1))
-    opt.dir = "./src/main/scala/org/sireum/bakar/sifl/module"
-    opt.genClassName = "BakarSiflModuleCore"
-    
-    ModuleGenerator.run(opt)
-  }
-}
+//package org.sireum.bakar.sifl.module
+//
+//import org.sireum.alir.ControlFlowGraph
+//import org.sireum.pipeline.Input
+//import org.sireum.pipeline.Produce
+//import org.sireum.option.PipelineMode
+//import org.sireum.pipeline.gen.ModuleGenerator
+//
+//case class BakarSiflConfig (
+//    title : String = "SIFL information flow analysis tool",
+//    
+//    @Input
+//    cfg : Boolean, //ControlFlowGraph[Boolean], 
+//    
+//    @Produce
+//    siflResults : Boolean
+//)
+//
+//object BakarSiflModuleGen {
+//  def main(args: Array[String]) {
+//    val opt = PipelineMode()
+//    opt.classNames = Array(BakarSiflConfig.getClass().getName().dropRight(1))
+//    opt.dir = "./src/main/scala/org/sireum/bakar/sifl/module"
+//    opt.genClassName = "BakarSiflModuleCore"
+//    
+//    ModuleGenerator.run(opt)
+//  }
+//}
